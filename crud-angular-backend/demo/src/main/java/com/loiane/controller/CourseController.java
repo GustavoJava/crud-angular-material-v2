dcf8c2d1bc7ef16d010c63be3fd13d1ed6b22afb
GustavoJava <gustavo.java2000@gmail.com>
2023-09-19 19:03:44 -0300

    findById

--- crud-angular-backend/demo/src/main/java/com/loiane/controller/CourseController.java
+++ crud-angular-backend/demo/src/main/java/com/loiane/controller/CourseController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,8 +27,15 @@ public class CourseController {
 	private final CourseRepository repository;
 	
 	@GetMapping
-	public List<Course> list() {
+	public  @ResponseBody List<Course> list() {
 		return this.repository.findAll(Sort.by(Sort.Direction.ASC, "name"));
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Course> findById(@PathVariable Long id) {
+		return this.repository.findById(id)
+				   .map(mapper -> ResponseEntity.ok().body(mapper))
+				   .orElse(ResponseEntity.notFound().build());
 	}
 	
 	@PostMapping
