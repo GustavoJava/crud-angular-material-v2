518d8889958c8098b2ae033d30d86523f9f2dd59
GustavoJava <gustavo.java2000@gmail.com>
2023-09-26 13:18:57 -0300

    update springboot 3.0.0

--- crud-angular-backend/demo/src/main/java/com/loiane/controller/CourseController.java
+++ crud-angular-backend/demo/src/main/java/com/loiane/controller/CourseController.java
@@ -2,9 +2,9 @@ package com.loiane.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
